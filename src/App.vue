0991d9201d5a08e20f8a9ce56087ae7589e2ca84
hexyzaiza@gmail.com <makita@dev-vuejs-test.c.ptna-20181002.internal>
2019-11-10 11:57:10 +0000

    [add]headerぽいのをつくる

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+  <div>
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        dark
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+        <v-toolbar-title>vrgames-index</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-content>
+        <HelloWorld/>
+      </v-content>
+    </v-card>
+  </div>
+
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld';
