9aacd6e1c819c46895a4417359b9304faf6ae4dd
hexyzaiza@gmail.com <makita@dev-vuejs-test.c.ptna-20181002.internal>
2019-11-10 13:19:19 +0000

    [refactor]eslintを実行する

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Index from './components/Index'
+import Vue from "vue";
+import Router from "vue-router";
+import Index from "./components/Index";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: 'http://34.84.154.202:8080/',
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index
-    }
-  ]
-})
+	mode: "history",
+	base: "http://34.84.154.202:8080/",
+	routes: [
+		{
+			path: "/",
+			name: "Index",
+			component: Index
+		}
+	]
+});
