e2606cfd936fc35272bd8f53bb44df4441aebf02
hexyzaiza@gmail.com <makita@dev-vuejs-test.c.ptna-20181002.internal>
2019-12-15 08:05:43 +0000

    [add]uikitをいれる

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 
+//UIKit
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+import 'uikit/dist/css/uikit.css'
+import 'uikit/dist/css/uikit.min.css'
+UIkit.use(Icons)
+    
+import './assets/main.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
