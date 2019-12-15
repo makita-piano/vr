cae8705ec61a5b03a3ab44e6e5db95e2387d9465
makita <hexyzaiza@gmail.com>
2019-12-15 08:07:17 +0000

    [add]固定jsonではなく、GSSを読み込む

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,11 +2,21 @@
   <VContainer pa-0>
     <VRow>
       <VCol
-        v-for="e in gamedata"
+        v-for="e in list"
         :key="e.id"
-        cols="6"
-        md="2"
+        cols="12"
+        md="12"
       >
+        <VRow>
+          <VCol
+            v-for="e in list"
+            :key="e.id"
+            cols="12"
+            md="12"
+          >
+            <p><span v-text="e.release_date"></span><span v-text="e.is_free"></span></p>
+          </VCol>
+        </VRow>
         <RouterLink
           to="/"
           exact
@@ -26,16 +36,14 @@ export default {
 	components: {},
 	data() {
 		return {
-			gamedata: require("../assets/json/gamedata.json"),
+			list: null,
 		};
 	},
 	mounted: function() {
-		window.addEventListener("resize", this.handleChange());
-	},
-	methods: {
-		getInfoLink(name) {
-			return "/a";
-		},
+	  axios
+      .get("https://script.googleusercontent.com/macros/echo?user_content_key=6SyehD-Rs73dBjpQN3wiOTddmknaHK4tAryz-VFL6ErMERLRynhgLg9O3Kyv93D_Y86bl3sPaFKxaMK1Hs1F5iXIWTyt-oeum5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnG8-oSvIL0Fqbtp9dmqBuJfBV9qbb_k4Bx7vI2fEuiROOs7obk4uz0a1FUPDiikXenaz7TzD4ctX&lib=MNlkLKR9rtqCw9OeFYV8adGX-bWlZsLTP") //ここにURLを入れる
+      .then(response => (this.list = response.data))
 	},
 };
+
 </script>
